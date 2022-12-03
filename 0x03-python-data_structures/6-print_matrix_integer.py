d636cca5a76bdb7ad9e45b760dabe02879e3f9e1
Funom3 <kingsleyisaackushang1997@gmail.com>
2022-12-03 12:03:54 -0800

    6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,4 @@
-#!/bin/python3
+#!/usr//bin/python3
 
 
 def print_matrix_integer(matrix=[[]]):
@@ -8,4 +8,4 @@ def print_matrix_integer(matrix=[[]]):
             if j != (len(matrix[i]) - 1):
                 print(" ", end="")
 
-            print("")
+            print()
