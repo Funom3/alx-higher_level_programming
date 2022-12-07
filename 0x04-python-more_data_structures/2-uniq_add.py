87ed4cad7fe18d47f41d268bec355eeca1a96212
Funom3 <kingsleyisaackushang1997@gmail.com>
2022-12-07 15:28:05 -0800

    2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -5,6 +5,6 @@ def uniq_add(my_list=[]):
     for x in my_list:
         if x not in unique_list:
             unique_list.append(x)
-    fpr x in unique_list:
+    for x in unique_list:
         result += x
     return result
