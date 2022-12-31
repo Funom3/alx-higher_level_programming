fcf259b9e77f9375d12201bf44f8d9c34ba0f33f
Funom3 <kingsleyisaackushang1997@gmail.com>
2022-12-31 10:45:44 -0800

    4-print_square.py

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Module composed by a function that prints 2 new lines after ".?:" characters
+"""
+
+
+def text_indention(text):
+    """ Function that prints 2 new lines after ".?:" characters
+    Args:
+        text: input string
+    Returns:
+        no return
+    Raises:
+        TypeError: if text is not a string
+    """
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
+
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
