5cade6aeed59a09deb77c647d87ef775729a2ca2
Funom# <kingsleyisaackushang1997@gmail.com>
2023-01-09 13:52:00 -0800

    7-base_geometry.py

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -12,12 +12,18 @@ class BaseGeometry:
         'area() is not omplemented'
         """
 
-        raise Exception('area() is not implemented')
+        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """validate the values"""
-
-        if type(value) is not int:
+        """validate a perimeter as an integer
+        Args:
+            name (str): the name
+            value (int): the parameter to validate
+        Raise:
+            TypeError: if value is not an integer
+            ValueError: if value is <= 0
+        """
+        if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
