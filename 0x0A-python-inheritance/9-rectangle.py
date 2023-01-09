d390859e31205defd0bc5b3faba17a6fa0130cac
Funom# <kingsleyisaackushang1997@gmail.com>
2023-01-09 12:44:36 -0800

    9-rectangle.py

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,40 @@
+#!usr/bin/python3
+"""module 9-rectangle
+creates a rectangle class
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Represents a rectangle
+    Private instances attributes:
+        - width
+        - height
+    Public method area()
+    Inherits from BaseGeometry
+    """
+
+    def __init__(self, width, height):
+        """initialize an instance
+        Args:
+            width: rectangle width
+            height: rectangle height
+        """
+
+        self.integer_validator("width", width)
+        sef.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+        def __str__(self):
+            """return a formated string"""
+            return str("[rectangle] {}/{}".format(self.__width, self.__height))
+
+        def area(self):
+            """computes the area of the rectangle instance
+            overwrites the area() method from the BaseGeometry
+            """
+
+            return self.__width * self.__height
