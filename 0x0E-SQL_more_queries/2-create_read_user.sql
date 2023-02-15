7524b836ea2b9ffc7d3f8b03e0c17ced584693e8
Funom3 <kingsleyisaackushang1997@gmail.com>
2023-02-15 11:53:28 -0800

    2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,10 +1,10 @@
 -- Creates the dartabase htbn_0d_2 and the user user_0d_2
 -- the user_0d_2 has SELECT privilege on hbtn_0d_2 with password user_0d_2_pwd
 CREATE DATABASE
-	IF NOT EXISTS `hbnt_0d_2`;
+     IF NOT EXISTS `hbnt_0d_2`;
 CREATE USER
-	IF NOT EXISTS "user_0d_2"@"localhost"
-	IDENTIFIED BY "user_0d_2_pwd";
+     IF NOT EXISTS "user_0d_2"@"localhost"
+     IDENTIFIED BY "user_0d_2_pwd";
 GRANT SELECT
-	ON `hbtn_0d_2`.*
-	TO "user_0d_2"@"localhost";
+     ON `hbtn_0d_2`.*
+     TO "user_0d_2"@"localhost";
