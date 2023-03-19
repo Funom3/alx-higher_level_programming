445d826f352823569cf5e5fdc186b57fca84c6d9
Funom3 <kingsleyisaackushang1997@gmail.com>
2023-03-19 16:00:16 -0700

    12-model_state_update_id_2.py

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -9,13 +9,13 @@ from sqlalchemy import asc
 
 
 if __name__ == "__main""__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhosrt/{}'.format
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
                            (sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     start = sessionmaker()
     start.configure(bind=engine)
     session = start()
-    stmt = session.quey(State).filter_by(id=2)
+    stmt = session.query(State).filter_by(id=2)
     for i in stmt:
         i.name = "New Mexico"
     session.commit()
